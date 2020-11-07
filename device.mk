32a66aeeae56c4dee43ec3e75051ce0f83d1fab4
Danny Lin <danny@kdrag0n.dev>
2020-11-06 21:13:36 -0800

    cepheus : Create more inodes for flexibility
    We routinely run out of inodes on /system if we limit it so tightly
    (~4096) due to ever-increasing amounts of files as more packages and
    libraries are added in newer versions of Android and downstream
    extensions.
    
    Instead of constantly having to increase the inode count, just set it to
    8192 using the existing PRODUCT_INCREASE_INODE_COUNT flag to fix the
    problems permanently. 8192 inodes should be more than enough for most
    purposes, and it's still low enough that the increased space usage is
    negligible.
    
    Change-Id: I59d866606fbdd06f0959a8c08db07a748532bdcb

--- device.mk
+++ device.mk
@@ -450,6 +450,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# Create more inodes for flexibility
+PRODUCT_INCREASE_INODE_COUNT := true
+
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
